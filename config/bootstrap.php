1ff98878e4bf36c5dac240e113c72dc7fc2a69d2
Rich Stone <richard.had@hotmail.de>
2016-11-29 08:29:36 +0100

    restore initial configs

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -213,7 +213,9 @@ if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
 */
-Plugin::load('DebugKit', ['bootstrap' => true]);
+if (Configure::read('debug')) {
+    Plugin::load('DebugKit', ['bootstrap' => true]);
+}
 
 Plugin::load('Migrations');
 
